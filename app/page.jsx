c84b681be1cfc446f3390213bdfd4b51077f662b
Vishal Kumar <21ucc116@lnmiit.ac.in>
2023-05-14 15:24:49 +0530

    initial commit

---
+++ app/page.jsx
@@ -0,0 +1,16 @@
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover and Share
+        <br className="" />
+        <span className="orange_gradient text-center">AI Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">Promptopia is an open-source AI prompting tool for modern world to discover and share creative prompts.</p>
+
+      {/* feed */}
+    </section>
+  );
+};
+
+export default Home;
